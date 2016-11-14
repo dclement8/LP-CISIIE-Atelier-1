5b56b4b059710327fb764b1c348d21cf364a2bc6
Thibaut Dalichampt <thibaut.dalichampt@outlook.com>
2016-11-14 16:50:00 +0100

    Photo maquettes
    Photo maquettes

---
+++ conf/jeu_essai.sql
@@ -0,0 +1,12 @@
+/* Table organisateur */
+INSERT INTO organisateur (login, mdp, nom, prenom, adresse, cp, ville, tel) 
+VALUES ('ADB', '$2y$10$fwYKrAIJuJx8QNVCNWB7NuEZLgwKZfT9ZziFUhrbjUdybBqRgIddW', 'Morris', 'Bob', '15 rue de l\alimentation Advance', '54000', 'Advance City', '03040506');
+
+INSERT INTO organisateur (login, mdp, nom, prenom, adresse, cp, ville, tel) 
+VALUES ('Bobybob', '$2y$10$fwYKrAIJuJx8QNVCNWB7NuEZLgwKZfT9ZziFUhrbjUdybBqRgIddW', 'Morris', 'John', '17 avenu du bracelet antistatique', '54000', 'banc de test City', '03040506');
+
+INSERT INTO organisateur (login, mdp, nom, prenom, adresse, cp, ville, tel) 
+VALUES ('ElectronikEart', '$2y$10$fwYKrAIJuJx8QNVCNWB7NuEZLgwKZfT9ZziFUhrbjUdybBqRgIddW', 'Fabien', 'Fabien', '12 impasse du téléphone chinois', '54000', 'AMD City', '03040506');
+
+/* Table organisateur */
+INSERT INTO 
