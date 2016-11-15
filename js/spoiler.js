5b5e79d3ea09beb1effb09135b43039487972a30
hitoshi54 <dylanclement7@gmail.com>
2016-11-15 14:50:31 +0100

    Revert "Structure pages ok et ajout du css"
    This reverts commit 9f7c8f8b7064c38f96a0d7718a82cab44eb62ff1.

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,11 +1,11 @@
 var spoiler = function(id) {
 	var div = document.querySelector("#spoiler-"+id);
 	if(div) {
-		if(div.style.display === "block") {
-			div.style.display = 'none';
+		if(div.style.display === "none") {
+			div.style.display = 'block';
 		}
 		else {
-			div.style.display = 'block';
+			div.style.display = 'none';
 		}
 	}
 }
