cdd2fbb1a1140800f81e25e267ae94eef719a917
Hugo <hugo-happe@hotmail.fr>
2016-11-15 11:03:22 +0100

    Méthodes des différentes classes
    Remplissage des classes + création des méthodes

--- src/sportnet/model/discipline.php
+++ src/sportnet/model/discipline.php
@@ -15,42 +15,103 @@ class discipline extends AbstractModel {
 	
 	protected function update()
 	{
-		
+		$update = ("UPDATE discipline SET nom = :nom WHERE id = :id");
+		$update_prep = self::$db->prepare($update);
+		$update_prep->bindParam(':nom', $this->nom, \PDO::PARAM_STR);
+		$update_prep->bindParam(':id', $this->id, \PDO::PARAM_INT);
+		if($update_prep->execute()){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
 	protected function insert()
 	{
-		
+		$insert = "INSERT INTO discipline VALUES(:id, :nom)";
+        $insert_prep = self::$db->prepare($insert);
+		$insert_prep->bindParam(':nom', $this->nom, \PDO::PARAM_STR);
+		$insert_prep->bindParam(':id', $this->id, \PDO::PARAM_INT);
+		if($insert_prep->execute()){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
 	public function save()
 	{
-		
+		if(is_null($this->id)){
+			return $this->insert();
+		}
+		else{
+			return $this->update();
+		}
 	}
 	
 	public function delete()
 	{
-		
+		$delete = "DELETE FROM discipline WHERE id = :id";
+        $delete_prep = self::$db->prepare($delete);
+		$delete_prep->bindParam(':id', $this->id, \PDO::PARAM_INT);
+		if($delete_prep->execute()){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
 	public static function findById($leId)
 	{
-		
+		$db = ConnectionFactory::makeConnection();
+        $selectById = "SELECT * FROM discipline WHERE id = :id";
+        $selectById_prep = self::$db->prepare($selectById);
+        $selectById_prep->bindParam(':id', $leId, \PDO::PARAM_INT);
+        if ($selectById_prep->execute()) {
+            return $selectById_prep->fetchObject(__CLASS__);
+        }else{
+            return null;
+        }
 	}
 	
 	public static function findAll()
 	{
-		
+		$db = ConnectionFactory::makeConnection();
+        $select = "SELECT * FROM discipline";
+        $resultat = self::$db->query($select);
+        if ($resultat) {
+            return $resultat->fetchAll(\PDO::FETCH_CLASS, __CLASS__);
+        }else{
+            return null;
+        }
 	}
 	
 	public static function findByName($leNom)
 	{
-		
+		$db = ConnectionFactory::makeConnection();
+        $selectByName = "SELECT * FROM discipline WHERE nom = :nom";
+        $selectByName_prep = self::$db->prepare($selectById);
+        $selectByName_prep->bindParam(':nom', $leNom, \PDO::PARAM_STR);
+        if ($selectByName_prep->execute()) {
+            return $selectByName_prep->fetchObject(__CLASS__);
+        }else{
+            return null;
+        }
 	}
 	
 	public function getEvenements()
 	{
-		
+		$select = "SELECT * FROM evenement where id = :id";
+        $select_prep = self::$db->prepare($select);
+        $select_prep->bindParam(":id", $this->id, \PDO::PARAM_INT);
+        if($select_prep->execute()){
+            return $select_prep->fetchObject(evenement::class);
+        }else{
+            return null;
+        }
 	}
 }
 ?>
