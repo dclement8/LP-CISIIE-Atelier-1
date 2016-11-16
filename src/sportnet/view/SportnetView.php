d3b2b8766b1002e613bea7c144030f9a67ddaff7
hitoshi54 <dylanclement7@gmail.com>
2016-11-16 09:37:37 +0100

    Ajout rendu creer et modification render

--- src/sportnet/view/SportnetView.php
+++ src/sportnet/view/SportnetView.php
@@ -18,6 +18,64 @@ class SportnetView extends AbstractView{
 		return $htmlRender;
 	}
 
+    protected function creer() {
+        $html  = "<div class='event offset-0 span-9'>\n";
+        $html .= "<h3>Evénement</h3>\n";
+        $html = <<<EOT
+<form method="post" action="creer.html">
+    <div class="event offset-0 span-9">
+        <h3>Evénement</h3>
+        <p><input type="text" name="nom" placeholder="Nom" required="required"></p>
+        <p>
+            <label>Discipline :</label>
+            <select name="discipline">
+                <option value="1">1</option>
+            </select>
+        </p>
+        <div>
+            Etat :
+            <p>
+                <label>Invisible :</label>
+                <input type="radio" name="etat" value="1" checked>
+            </p>
+            <p>
+                <label>Visible (Inscription fermées) :</label>
+                <input type="radio" name="etat" value="2">
+            </p>
+            <p>
+                <label>Visible (Inscription ouvertes) :</label>
+                <input type="radio" name="etat" value="3">
+            </p>
+        </div>
+        <p>
+            <label>Date limite d''inscription :</label>
+            <input type="datetime-local" name="date" required="required">
+        </p>
+        <p>
+            <label>Tarif :</label>
+            <input type="number" name="tarif" min="0" required="required"> €
+        </p>
+        <p>
+            <label>Description :</label>
+            <textarea name="description" required="required"></textarea>
+        </p>
+    </div>
+
+    <div class="event offset-0 span-3">
+        <h3>Ajouter une épreuve</h3>
+        <p><input type="text" name="nom_epreuve" placeholder="Nom" required="required"></p>
+        <p>
+            <label>Date :</label>
+            <input type="date" name="date_epreuve" required="required">
+        </p>
+        <p><input type="number" name="dist_epreuve" placeholder="Distance (en m)" min="1" max="100000" required="required"></p>
+        <p><input type="submit" value="Créer"></p>
+    </div>
+</form>
+EOT;
+        return $html;
+    }
+
 
     /*
      * Affiche une page HTML complète.
@@ -27,12 +85,35 @@ class SportnetView extends AbstractView{
      */
     public function render($selector){
         switch($selector){
-			case 'view':
-				$main = $this->method();
+			case 'creer':
+                $breadcrumb = $this->renderBreadcrumb(array('Créer un événement'));
+				$main = $this->creer();
 				break;
 
+            case 'details_ouvert':
+                $breadcrumb = $this->renderBreadcrumb();
+    			$main = $this->method();
+    			break;
+
+            case 'details_ferme':
+        		$main = $this->method();
+                $breadcrumb = $this->renderBreadcrumb();
+        		break;
+
+            case 'espace':
+                $breadcrumb = $this->renderBreadcrumb();
+                //$main = $this->espace();
+                break;
+
+            case 'mes_events':
+                $breadcrumb = $this->renderBreadcrumb();
+                //$main = $this->method();
+        		break;
+
 			default:
-				$main = $this->method();
+                // Liste des events
+                $breadcrumb = $this->renderBreadcrumb();
+                //$main = $this->method();
 				break;
         }
 
@@ -40,8 +121,6 @@ class SportnetView extends AbstractView{
 
         $header 	= $this->renderHeader();
         $menu   	= $this->renderMenu();
-		$breadcrumb = $this->renderBreadcrumb();
-
 
 /*
  * Utilisation de la syntaxe HEREDOC pour écrire la chaine de caractère de
